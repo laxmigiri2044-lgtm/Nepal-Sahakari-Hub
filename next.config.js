2b25f1b5cc3474bf768e13ff98961f1a1900a52a
Nepal Sahakari Hub <admin@nepalsahakarihub.com>
2026-05-12 21:26:08 +0545

    Externalize Node.js modules for MongoDB in webpack config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {},
+  webpack: (config) => {
+    config.externals = config.externals || []
+    config.externals.push('net', 'tls', 'child_process', 'fs/promises')
+    return config
+  },
 }
 
 module.exports = nextConfig
